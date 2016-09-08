55a9cb302fc36c4ddcdaed88beaf5f01b4876d91
esampsonite <esampsonite.github@sampson.tv>
2016-09-07 20:28:42 -0500

    You can search for sets again

---
+++ WindmillHelix.Brickficiency2.Common/Utils/ItemTypeUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindmillHelix.Brickficiency2.Common.Utils
+{
+    public static class ItemTypeUtil
+    {
+        public static bool DoesComeInColors(string itemTypeCode)
+        {
+            return itemTypeCode == ItemTypeCodes.Part || itemTypeCode == ItemTypeCodes.Gear;
+        }
+    }
+}
